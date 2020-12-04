780c1a3b9903bcb6f4f80100e9aaf483f0f75c12
Jonathan Tweedy <jetweedy@users.noreply.github.com>
2020-12-04 13:10:30 -0500

    Add files via upload

--- blank.js
+++ blank.js
@@ -1,7 +1,6 @@
 
 window.addEventListener("load", function() {
     setTimeout(function() {
-//        window.location.replace("https://jonathantweedy.com");
         loadBookmarks();
     }, 100);
 });
