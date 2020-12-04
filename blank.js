4f8b0ea452f250965d07fcd4ac732979d6b42458
Jonathan Tweedy <jetweedy@users.noreply.github.com>
2020-12-04 14:08:13 -0500

    Add files via upload

--- blank.js
+++ blank.js
@@ -1,8 +1,10 @@
 
-window.addEventListener("load", function() {
-    setTimeout(function() {
-        loadBookmarks();
-    }, 100);
-});
-
-
+/*
+let script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = 'https://www.cranialmeltdown.com/portal/jet-tabcheck.js';    
+document.getElementsByTagName('head')[0].appendChild(script);
+script.onload = function() {
+    console.log("jet-tabcheck installed!");
+}
+*/
